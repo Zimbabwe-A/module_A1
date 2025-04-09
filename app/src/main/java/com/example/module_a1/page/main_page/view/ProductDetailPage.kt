9e7bd2f3010e550f7d62bc0aa3c899eaefe1f070
Zimbabwe-A <marchenko.artem06@mail.ru>
2025-04-09 14:18:22 +0500

    изменил дизайн как на проекте

--- app/src/main/java/com/example/module_a1/page/main_page/view/ProductDetailPage.kt
+++ app/src/main/java/com/example/module_a1/page/main_page/view/ProductDetailPage.kt
@@ -1,51 +1,135 @@
 package com.example.module_a1.page.main_page.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.module_a1.page.main_page.model.Product
 import com.example.module_a1.page.main_page.CartManager
+import com.example.module_a1.ui.theme.Gray100
+import com.example.module_a1.ui.theme.Purple
 
 @Composable
 fun ProductDetailPage(product: Product, navController: NavController) {
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .background(Gray100)
     ) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp)
+                .height(400.dp)
                 .background(Color.Blue),
             contentAlignment = Alignment.Center
         ) {
-            Text(text = product.name, color = Color.White)
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "Цена: ${product.price}₽", style = MaterialTheme.typography.headlineSmall)
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Text(text = product.description, style = MaterialTheme.typography.bodyMedium)
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Button(
-            onClick = {
-                CartManager.CartManager.addToCart(product)
-                navController.popBackStack()
-            }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
         ) {
-            Text("Купить")
+            Text(
+                text = product.name,
+                style = TextStyle(
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 23.sp,
+                )
+            )
+
+            Text(
+                text = "${product.price} ТГ",
+                style = TextStyle(
+                    color = Color.Gray,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color.LightGray),
+            ) {
+                Column(
+                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 5.dp)
+                ) {
+                    Text(
+                        text = "Описание:",
+                        style = TextStyle(
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp,
+                        )
+                    )
+                    Text(text = product.description, style = MaterialTheme.typography.bodyMedium)
+                }
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp)
+                    .height(40.dp)
+                    .clip(
+                        RoundedCornerShape(8.dp)
+                    )
+                    .background(Purple)
+                    .clickable {
+                        CartManager.CartManager.addToCart(product)
+                        navController.popBackStack()
+                    },
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(
+                    text = "Добавить в корзину",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+        }
+    }
+    Box(
+        modifier = Modifier
+            .padding(top = 5.dp, start = 5.dp)
+            .clip(RoundedCornerShape(50.dp))
+            .background(Color.Gray)
+            .graphicsLayer(alpha = 0.5f)
+    ) {
+        IconButton(onClick = {navController.popBackStack()}) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "Назад",
+                tint = Color.Black
+            )
         }
     }
 }
